df72ffc9cd59bb310dac57dbf1d7a5fc54b9b86c
nikitsya <nikitasmiichyk@gmail.com>
2026-04-05 00:28:49 +0100

    Add the site icon as favicon from the public directory

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Orange Retail | Login</title>
+        <link rel="icon" type="image/png" href="{{ asset('icon.png') }}">
         <link rel="stylesheet" href="{{ asset('css/orange-market.css') }}">
     </head>
     <body>
--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Orange Retail | Catalog</title>
+        <link rel="icon" type="image/png" href="{{ asset('icon.png') }}">
         <link rel="stylesheet" href="{{ asset('css/orange-market.css') }}">
     </head>
     <body>
