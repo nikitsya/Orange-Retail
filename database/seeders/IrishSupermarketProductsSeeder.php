10689f80a8e93d4ba938b509278f2c235ef87cc8
nikitsya <nikitasmiichyk@gmail.com>
2026-04-06 21:57:11 +0100

    Use category-based supermarket-scale stock generation

--- database/seeders/IrishSupermarketProductsSeeder.php
+++ database/seeders/IrishSupermarketProductsSeeder.php
@@ -32,6 +32,25 @@ class IrishSupermarketProductsSeeder extends Seeder
 
         $products = collect($payload['products'])
             ->map(function (array $product): array {
+                [
+                    'minimum_range' => [$minimumRangeStart, $minimumRangeEnd],
+                    'healthy_range' => [$healthyRangeStart, $healthyRangeEnd],
+                ] = $this->inventoryProfile((string) $product['category']);
+
+                $minimumStockLevel = random_int($minimumRangeStart, $minimumRangeEnd);
+                $inventoryStateRoll = random_int(1, 100);
+
+                if ($inventoryStateRoll <= 10) {
+                    $stock = random_int(max(0, $minimumStockLevel - 20), max(0, $minimumStockLevel - 1));
+                } elseif ($inventoryStateRoll <= 18) {
+                    $stock = $minimumStockLevel;
+                } else {
+                    $stock = random_int(
+                        max($minimumStockLevel + 5, $healthyRangeStart),
+                        max($minimumStockLevel + 20, $healthyRangeEnd),
+                    );
+                }
+
                 return [
                     'sku' => (string)$product['sku'],
                     'barcode' => $product['barcode'] ?: null,
@@ -46,8 +65,8 @@ class IrishSupermarketProductsSeeder extends Seeder
                         ? (float)$product['price_value']
                         : null,
                     'unit_price_display' => $product['unit_price_display'] ?: null,
-                    'stock' => 4 + ((((int)($product['id'] ?? 0)) * 3) % 28),
-                    'minimum_stock_level' => 5 + ((((int)($product['id'] ?? 0)) % 4) * 2),
+                    'stock' => $stock,
+                    'minimum_stock_level' => $minimumStockLevel,
                     'is_active' => true,
                     'last_restocked_at' => now()->subDays((((int)($product['id'] ?? 1)) % 5) + 1),
                     'next_delivery_due_at' => now()->addDays((((int)($product['id'] ?? 1)) % 6) + 2),
@@ -78,5 +97,54 @@ class IrishSupermarketProductsSeeder extends Seeder
                 ],
             );
         }
+    }
+
+    /**
+     * @return array{minimum_range: array{int, int}, healthy_range: array{int, int}}
+     */
+    protected function inventoryProfile(string $category): array
+    {
+        return match ($category) {
+            'Fresh Food' => [
+                'minimum_range' => [20, 45],
+                'healthy_range' => [55, 130],
+            ],
+            'Drinks' => [
+                'minimum_range' => [30, 70],
+                'healthy_range' => [90, 220],
+            ],
+            'Food Cupboard' => [
+                'minimum_range' => [35, 80],
+                'healthy_range' => [100, 240],
+            ],
+            'Treats & Snacks' => [
+                'minimum_range' => [28, 65],
+                'healthy_range' => [85, 190],
+            ],
+            'Household' => [
+                'minimum_range' => [20, 50],
+                'healthy_range' => [60, 150],
+            ],
+            'Pets' => [
+                'minimum_range' => [16, 40],
+                'healthy_range' => [45, 120],
+            ],
+            'Health & Beauty' => [
+                'minimum_range' => [14, 35],
+                'healthy_range' => [35, 90],
+            ],
+            'Baby & Toddler' => [
+                'minimum_range' => [16, 42],
+                'healthy_range' => [45, 110],
+            ],
+            'Home & Furniture' => [
+                'minimum_range' => [6, 18],
+                'healthy_range' => [15, 45],
+            ],
+            default => [
+                'minimum_range' => [20, 45],
+                'healthy_range' => [50, 120],
+            ],
+        };
     }
 }
